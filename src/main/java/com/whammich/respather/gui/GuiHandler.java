f27b518233a6106a4ed9307c6b2a5656c481df67
SgtPunishment <sgtpunishment@gmail.com>
2015-04-29 23:39:01 +0100

    More work on the gui stuff

--- src/main/java/com/whammich/respather/gui/GuiHandler.java
+++ src/main/java/com/whammich/respather/gui/GuiHandler.java
@@ -7,18 +7,20 @@ import cpw.mods.fml.common.network.IGuiHandler;
 
 public class GuiHandler implements IGuiHandler {
 
+
+	
 	public GuiHandler() {
 
 	}
 
 	@Override
 	public Object getServerGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
-		return ID == 0 ? new ContainerBag(player.inventory) : null;
+		return ID == 0 ? new ContainerBag(player.inventory, new InventoryBag()) : null;
 	}
 
 	@Override
 	public Object getClientGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
-		return ID == 0 ? new GuiBasicBag(player.inventory) : null;
+		return ID == 0 ? new GuiBasicBag(player.inventory, new InventoryBag()) : null;
 				
 				//GuiBasicBag(player.getCurrentEquippedItem(), player) : null;
 	}
