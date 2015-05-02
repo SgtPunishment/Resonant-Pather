57d20520bc6bc9cab1cf970270ac23650f236b64
SgtPunishment <sgtpunishment@gmail.com>
2015-05-02 21:03:20 +0100

    All the mod loggers!

--- src/main/java/com/whammich/resrandom/gui/InventoryBag.java
+++ src/main/java/com/whammich/resrandom/gui/InventoryBag.java
@@ -5,6 +5,7 @@ import net.minecraft.inventory.IInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.nbt.NBTTagList;
+import net.minecraftforge.common.util.Constants;
 
 import com.whammich.resrandom.items.ItemBaseBag;
 import com.whammich.resrandom.utils.ModLogger;
@@ -19,17 +20,12 @@ public class InventoryBag implements IInventory {
 	private final ItemStack invItem;
 
 	public InventoryBag(ItemStack stack) {
-		
 		invItem = stack;
-		
 		if(!stack.hasTagCompound()){
 			stack.setTagCompound(new NBTTagCompound());
 		}
-		
 		readFromNBT(stack.getTagCompound());
-		
 		ModLogger.logInfo("01: Reading Stack: " + stack.toString());
-
 	}
 	
 	@Override
@@ -91,13 +87,15 @@ public class InventoryBag implements IInventory {
 	public void markDirty() {
 		for (int i = 0; i < getSizeInventory(); ++i){
 			if(getStackInSlot(i) != null && getStackInSlot(i).stackSize == 0){
-				bagContents[i] = null;
+				setInventorySlotContents(i, null);
+				//bagContents[i] = null;
 			}
 		}
 //		NBTTagCompound tag = new NBTTagCompound();
 //		writeToNBT(tag);
 //		invItem.setTagCompound(tag);
 		writeToNBT(invItem.getTagCompound());
+		ModLogger.logInfo("markDirt(): " + invItem.getTagCompound());
 	}
 
 	@Override
@@ -117,21 +115,16 @@ public class InventoryBag implements IInventory {
 	}
 
 	public void readFromNBT(NBTTagCompound compound) {
-		NBTTagList items = compound.getTagList("Items", 10);
+		NBTTagList items = compound.getTagList("Items", Constants.NBT.TAG_COMPOUND);
 		for (int i = 0; i < items.tagCount(); ++i) {
-			
 			NBTTagCompound item = items.getCompoundTagAt(i);
-			ModLogger.logInfo("02: Reading Item: " + item.toString());
-			
+			ModLogger.logInfo("Pre-Slot: " + i + " :" + item.toString());
 			int slot = item.getInteger("Slot");
-			ModLogger.logInfo("03: Reading Item: " + item.toString());
-			
+			ModLogger.logInfo("Post-Slot: " + i + " :" + item.toString());			
 			if (slot >= 0 && slot < this.bagContents.length) {
-				ModLogger.logInfo("04: Reading Item: " + item.toString());
-				
+				ModLogger.logInfo("If-Slot: " + i + " :" + item.toString());
 				this.bagContents[slot] = ItemStack.loadItemStackFromNBT(item);
-				ModLogger.logInfo("05: Reading Item: " + item.toString());
-
+				ModLogger.logInfo("Done-Slot: " + i + " :" + item.toString());
 			}
 		}
 	}
@@ -140,14 +133,15 @@ public class InventoryBag implements IInventory {
 		NBTTagList items = new NBTTagList();
 		
 		for (int i = 0; i < getSizeInventory(); ++i) {
-			
+			ModLogger.logInfo("Null-Slot: " + i + " :" + items.toString());
 			if (getStackInSlot(i) != null) {
-				
+				ModLogger.logInfo("Tag-Slot: " + i + " :" + items.toString());
 				NBTTagCompound item = new NBTTagCompound();
 				item.setInteger("Slot", i);
-				
+				ModLogger.logInfo("Pre-Slot: " + i + " :" + items.toString());
 				getStackInSlot(i).writeToNBT(item);
 				items.appendTag(item);
+				ModLogger.logInfo("Post-Slot: " + i + " :" + items.toString());
 			}
 		}
 		ModLogger.logInfo("Saving Item: " + items.toString());
